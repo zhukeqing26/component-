8c49cc34b6159162ef51b146caa3e71c0d6ee968
zhukeqing <zhukeqing26@163.com>
2024-03-07 21:30:55 +0800

    feat: 增加组件库

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import '@/assets/style/index.sass'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      imports: ["vue", "vue-router", "pinia"],
-      dts: "src/types/auto-import.d.ts", // 路径下自动生成文件夹存放全局指令
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: 'src/types/auto-import.d.ts', // 路径下自动生成文件夹存放全局指令
     }),
     Components({
       resolvers: [
-        //这里配置解析器，具体参数官网有解释
+        //这里配置解析器
+        ElementPlusResolver({ importStyle: false }),
       ],
-      dts: "src/types/components.d.ts",
+      dts: 'src/types/components.d.ts',
     }),
   ],
-  resolve:{
-    alias:{
-      '@':resolve(__dirname,'src')
-    }
-  }
-});
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+})
